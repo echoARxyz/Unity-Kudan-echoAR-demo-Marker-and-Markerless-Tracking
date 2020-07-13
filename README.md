b64f21f8e8be1e64054454e3c6ec425bddad8dfa
bpedazur <bpedazur@gmail.com>
2020-07-13 12:06:27 -0400

    Added project files

---
+++ README.md
@@ -0,0 +1,34 @@
+# Unity-Kudan-echoAR-demo-Marker-and-Markerless-Tracking
+Example demo that uses marker and markerless tracking with Unity, Kudan, and echoAR.
+
+## Register
+Don't have an API key? Make sure to register for FREE at [echoAR](https://console.echoar.xyz/#/auth/register).
+
+## Setup
+* Download and install the Kudan SDK in a new Unity project (note: Kudan only works on Unity 2018 and earlier).
+* Enter your Kudan API Key into the Kudan Camera object on the Unity Editor.
+* Create a new scene and add the Angel Bundle from KudanAR/Prefabs.
+* On the MarkerTracking object nested below Kudan Camera, press "Add KARMarker Asset" and select ImageMarkers.KARMarker from the [Assets folder](/Assets).
+* Click the Markers dropdown arrow within MarkerTracking to show the list of Kudan Markers. Replace the existing marker with the NewKudanTrackable object that was created.
+* Install the echoAR SDK into the project.
+* Follow the instructions on our [doumention page](https://docs.echoar.xyz/unity/adding-ar-capabilities) to [set your API key](https://docs.echoar.xyz/unity/adding-ar-capabilities#3-set-you-api-key).
+* Overwrite the exisitng _echoAR/CustomBehavior.cs script with the new [CustomBehavior](https://github.com/echoARxyz/Unity-ARFoundation-echoAR-demo-Living-Room-TV/blob/master/CustomBehavior.cs) file.
+* Replace the angelStatue in Drivers/Marker and the Capsule in Drivers/Markerless with two echoAR prefabs.
+* Add an apartment model into the echoAR console and use the metadata from Metadata.csv.
+
+
+## Run
+* [Build and run the AR application](https://docs.echoar.xyz/unity/adding-ar-capabilities#4-build-and-run-the-ar-application).
+* To change which image is being tracked at runtime for marker tracking, replace the "imageKey" attribute in the console with either "clouds", "lego", or "leaves" for the corresponding image in the [Images folder](/Images).
+
+
+## Learn more
+Refer to our [documentation](https://docs.echoar.xyz/unity/) to learn more about how to use Unity and echoAR.
+
+## Support
+Feel free to reach out at [support@echoAR.xyz](mailto:support@echoAR.xyz) or join our [support channel on Slack](https://join.slack.com/t/echoar/shared_invite/enQtNTg4NjI5NjM3OTc1LWU1M2M2MTNlNTM3NGY1YTUxYmY3ZDNjNTc3YjA5M2QyNGZiOTgzMjVmZWZmZmFjNGJjYTcxZjhhNzk3YjNhNjE). 
+
+## Screenshots
+![console](/Screenshots/echoAR_Console.png)
+![marker](/Screenshots/Marker.gif)
+![merkerless](/Screenshots/Markerless.gif)
---
+++ Scripts/CustomBehaviour.cs
@@ -0,0 +1,75 @@
+﻿/**************************************************************************
+* Copyright (C) echoAR, Inc. 2018-2020.                                   *
+* echoAR, Inc. proprietary and confidential.                              *
+*                                                                         *
+* Use subject to the terms of the Terms of Service available at           *
+* https://www.echoar.xyz/terms, or another agreement                      *
+* between echoAR, Inc. and you, your company or other organization.       *
+***************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kudan.AR;
+
+public class CustomBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public Entry entry;
+
+    static GameObject KudanCamera = GameObject.Find("Kudan Camera");
+    static KudanTracker Tracker = KudanCamera.GetComponent<KudanTracker>();
+
+    static GameObject Marker = GameObject.Find("Marker");
+    static MarkerTransformDriver Driver = Marker.GetComponent<MarkerTransformDriver>();
+
+    /// <summary>
+    /// EXAMPLE BEHAVIOUR
+    /// Queries the database and names the object based on the result.
+    /// </summary>
+
+    // Use this for initialization
+    void Start()
+    {
+        // Add RemoteTransformations script to object and set its entry
+        
+
+        // Qurey additional data to get the name
+        string value = "";
+        
+        
+
+        TrackingMethodBase method = Tracker.CurrentTrackingMethod;
+        if (method is TrackingMethodMarker) {
+            this.gameObject.transform.rotation = Quaternion.Euler(-90,0,0);
+        }
+        else {
+            this.gameObject.transform.rotation = Quaternion.Euler(0.0f, -25.0f, 90.0f);
+        }
+
+        if (entry.getAdditionalData().TryGetValue("imageKey", out value)) {
+            Driver._expectedId = value;
+        }
+
+        this.gameObject.AddComponent<RemoteTransformations>().entry = entry;
+        if (entry.getAdditionalData() != null && entry.getAdditionalData().TryGetValue("name", out value))
+        {
+            // Set name
+            this.gameObject.name = value;
+        }
+
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = "";
+
+        if (entry.getAdditionalData().TryGetValue("imageKey", out value)) {
+            Driver._expectedId = value;
+        }
+
+    }
+}
